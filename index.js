426d215c5395d13c911a3d6f955e8a7b0d074615
justinsavage49 <justinsavage49@gmail.com>
2023-06-30 00:04:06 -0700

    Revert "refactor"
    This reverts commit b8aa23f296b05dcffe4d386e26f6a1c463580aab.

--- index.js
+++ index.js
@@ -1,15 +1,97 @@
 document.addEventListener('DOMContentLoaded', main)
 
 function main() {
-	const asciiString1 = `¶@ØÆMåBNÊßÔR#8Q&mÃ0À$GXZA5ñk2S%±3Fz¢yÝCJf1t7ªLc¿+?(r/¤²!*;"^:,'.`;
-	const asciiString2 = `╬╠╫╋║╉╩┣╦╂╳╇╈┠╚┃╃┻╅┳┡┢┹╀╧┱╙┗┞┇┸┋┯┰┖╲╱┎╘━┭┕┍┅╾│┬┉╰╭╸└┆╺┊─╌┄┈╴╶`;
-	const ascii_output = document.getElementById("ascii_output");
 	const image_input = document.getElementById("image_input");
+	const canvas = document.getElementById("canvas");
+	const ctx = canvas.getContext("2d");
+	const pixelDataElement = document.getElementById("pixel_data");
+	const masterScale = 100;
+	
+	const asciiString = `¶@ØÆMåBNÊßÔR#8Q&mÃ0À$GXZA5ñk2S%±3Fz¢yÝCJf1t7ªLc¿+?(r/¤²!*;"^:,'.`;
 
-	generatePhoto(image_input)
-	.then((processedArray) => {
-		arrayToAscii(asciiString1, processedArray, ascii_output);
-	});
+	//Alternative characters look better on lower contrast photos.
+	// const asciiString = `╬╠╫╋║╉╩┣╦╂╳╇╈┠╚┃╃┻╅┳┡┢┹╀╧┱╙┗┞┇┸┋┯┰┖╲╱┎╘━┭┕┍┅╾│┬┉╰╭╸└┆╺┊─╌┄┈╴╶`;
+
+	const asciiArray = asciiString.split("");
+	asciiArray.reverse();
+	const brightnessScale = 255 / (asciiArray.length - 1);
+	
+	image_input.addEventListener("change", (event) => {
+		const file = event.target.files[0];
+		const reader = new FileReader();
+		
+		reader.onload = function () {
+			const image = new Image();
+			
+			image.onload = function () {
+				canvas.width = image.width;
+				canvas.height = image.height;
+				
+				ctx.drawImage(image, 0, 0, image.width, image.height);
+				
+				const imageData = ctx.getImageData(0, 0, image.width, image.height);
+				const pixelData = imageData.data;
+				
+				grayscaleConversion(pixelData)     
+				ctx.putImageData(imageData, 0, 0);
+				
+				let xScale = Math.round(canvas.width / masterScale);
+				let xValues = 0;
+				let xValueIndex = 0;
+				const xScaledArray = [];
+				
+				for (let y = 0; y < canvas.height; y++) {
+					const xPixels = [];
+					
+					for (let x = 0; x < canvas.width; x++) {
+						const pixelIndex = (y * canvas.width + x) * 4;
+						const r = pixelData[pixelIndex];
+						xValues += r;
+						xValueIndex += 1;
+						
+						if(xValueIndex == xScale) {
+							const avgX = Math.floor(xValues / xValueIndex);
+							xPixels.push(avgX);
+							xValues = 0;
+							xValueIndex = 0;
+						}
+						else if (xValueIndex + x >= canvas.width) {
+							const avgX = Math.floor(xValues / xValueIndex);
+							xPixels.push(avgX);
+						}
+					}
+					
+					xScaledArray.push(xPixels);
+				}
+
+				const yArray = invertArray(xScaledArray);
+				const yScaledArray = scalePixels(yArray, yArray.length, yArray[0].length, canvas.width, masterScale * .45);
+				const scaledArray = invertArray(yScaledArray, true);
+
+				const getAsciiCharacter = function(brightness) {
+					if(brightness === "\n") { 
+						return brightness; 
+					}
+					const index = Math.floor(brightness / brightnessScale);
+					
+					return asciiArray[index];
+				}
+				
+				const asciiOutput = [];
+				for(x = 0; x< scaledArray.length; x++) {
+					for(y = 0; y< scaledArray[0].length; y++) {
+						asciiOutput.push(getAsciiCharacter(scaledArray[x][y]));
+					}
+				}
+				
+				pixelDataElement.textContent = asciiOutput.join("");
+			};
+			
+			image.src = reader.result;
+		};
+		
+		reader.readAsDataURL(file);
+	}, false);
 }
 
 
--- utils.js
+++ utils.js
@@ -1,72 +1,3 @@
-function generatePhoto(inputElement) {
-
-	return new Promise((resolve) => {
-		const canvas = document.getElementById("canvas");
-		const ctx = canvas.getContext("2d");
-		const masterScale = 100;
-		
-		inputElement.addEventListener("change", (event) => {
-			const file = event.target.files[0];
-			const reader = new FileReader();
-			
-			reader.onload = function () {
-				const image = new Image();
-				
-				image.onload = function () {
-					canvas.width = image.width;
-					canvas.height = image.height;
-					
-					ctx.drawImage(image, 0, 0, image.width, image.height);
-					
-					const imageData = ctx.getImageData(0, 0, image.width, image.height);
-					const pixelData = imageData.data;
-					
-					grayscaleConversion(pixelData)     
-					ctx.putImageData(imageData, 0, 0);
-					
-					let xScale = Math.round(canvas.width / masterScale);
-					let xValues = 0;
-					let xValueIndex = 0;
-					const xScaledArray = [];
-					
-					for (let y = 0; y < canvas.height; y++) {
-						const xPixels = [];
-						
-						for (let x = 0; x < canvas.width; x++) {
-							const pixelIndex = (y * canvas.width + x) * 4;
-							const r = pixelData[pixelIndex];
-							xValues += r;
-							xValueIndex += 1;
-							
-							if(xValueIndex == xScale) {
-								const avgX = Math.floor(xValues / xValueIndex);
-								xPixels.push(avgX);
-								xValues = 0;
-								xValueIndex = 0;
-							}
-							else if (xValueIndex + x >= canvas.width) {
-								const avgX = Math.floor(xValues / xValueIndex);
-								xPixels.push(avgX);
-							}
-						}
-						
-						xScaledArray.push(xPixels);
-					}
-
-					const yArray = invertArray(xScaledArray);
-					const yScaledArray = scalePixels(yArray, yArray.length, yArray[0].length, canvas.width, masterScale * .45);
-
-					resolve(invertArray(yScaledArray, true));
-				};
-				
-				image.src = reader.result;
-			};
-			
-			reader.readAsDataURL(file);
-		}, false);
-	});
-}
-
 function invertArray(array, lineBreak=false) {
 	const a = [];
 
@@ -131,28 +62,8 @@ function grayscaleConversion(pixelData) {
 		pixelData[i + 1] = grayscale;
 		pixelData[i + 2] = grayscale;
 	}
-}	
+}
 
-function arrayToAscii(asciiString, processedArray, htmlOutput) {
-	const asciiArray = asciiString.split("");
-	asciiArray.reverse();
-	const brightnessScale = 255 / (asciiArray.length - 1);
-
-	const getAsciiCharacter = function(brightness) {
-		if(brightness === "\n") { 
-			return brightness; 
-		}
-		const index = Math.floor(brightness / brightnessScale);
-		
-		return asciiArray[index];
-	}
-
-	const asciiOutput = [];
-	for(x = 0; x < processedArray.length; x++) {
-		for(y = 0; y < processedArray[0].length; y++) {
-			asciiOutput.push(getAsciiCharacter(processedArray[x][y]));
-		}
-	}
-
-	htmlOutput.textContent = asciiOutput.join("");
+function debug(array, callback, getAsciiCharacter) {
+	callback(array.map(value => getAsciiCharacter(value)))
 }
